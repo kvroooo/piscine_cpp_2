975740869f23ec3c2ac0339262b0bb651b7a38f3
Samira <kvrosam@gmail.com>
2024-07-04 19:18:30 +0200

    ugh

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: smlamali <smlamali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 13:56:44 by smlamali          #+#    #+#             */
-/*   Updated: 2024/07/01 17:49:14 by smlamali         ###   ########.fr       */
+/*   Updated: 2024/07/04 19:06:50 by smlamali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 
 #include <iostream>
 #include <stack>
+#include <deque>
 
-template<typename T> class MutantStack; 
-
-template <typename T>
-class MutantStack
+template<class T, class Container = std::deque<T>>
+class MutantStack : public std::stack<T>
 {
-private:
-	std::stack<T> _s;
 public:
-	MutantStack<T>(){std::cout << "constructor called" << std::endl;};
+	MutantStack(){std::cout << "constructor called" << std::endl;}
 	~MutantStack(){}
-	MutantStack<T>(const MutantStack<T> & m);
+	MutantStack<T>(const MutantStack<T> & m)
+	{*this = m;};
 
 	MutantStack<T> & operator=(const MutantStack<T> & m);
 
-	T & 	top(void)const;
-	bool	empty(void)const;
-	size_t	size(void)const;
-	void	push(const T & val);
-	void	pop(void);
+	typedef typename std::stack<T>::container::iterator it; 
+	
+	// T & 	top(void)const;
+	// bool	empty(void)const;
+	// size_t	size(void)const;
+	// void	push(const T & val);
+	// void	pop(void);
 };
 
 #include "MutantStack.tpp"
--- cpp08/ex02/MutantStack.tpp
+++ cpp08/ex02/MutantStack.tpp
@@ -6,27 +6,27 @@
 /*   By: smlamali <smlamali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 13:53:09 by smlamali          #+#    #+#             */
-/*   Updated: 2024/07/01 17:49:32 by smlamali         ###   ########.fr       */
+/*   Updated: 2024/07/04 17:50:23 by smlamali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "MutantStack.hpp"
+// #include "MutantStack.hpp"
 
-MutantStack<T>::MutantStack(const MutantStack<T> & m)
-{
-	std::cout << "copy constructor called !" << std::endl;
-}
+// MutantStack<T>::MutantStack(const MutantStack<T> & m)
+// {
+// 	std::cout << "copy constructor called !" << std::endl;
+// }
 
-MutantStack<T> & MutantStack<T>::operator=(const MutantStack<T> & m)
-{
-	std::cout << "copy assignment operator called !" << std::endl;
-	(void)m;
-	return *this;
-}
+// MutantStack<T> & MutantStack<T>::operator=(const MutantStack<T> & m)
+// {
+// 	std::cout << "copy assignment operator called !" << std::endl;
+// 	(void)m;
+// 	return *this;
+// }
 
-template <typename T>
-T & MutantStack<T>::top(void)const
-{
-	std::cout << _s.top() << std::endl;
-}
+// template <typename T>
+// T & MutantStack<T>::top(void)const
+// {
+// 	std::cout << _s.top() << std::endl;
+// }
 
